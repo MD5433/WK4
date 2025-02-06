0a3ab7743f0c7912ba54050e2aa845c134071ab7
111513 <mariahdungey01@gmail.com>
2025-02-06 14:20:55 -0500

    notes for this week

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.*;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int[] arr = new int[3];
+        arr[0] = 1;
+        arr[1] = 17;
+        arr[2] = 35;
+
+        for (int i = 0; i < 4; i++){
+            System.out.println(Arrays.toString(arr));
+        }
+
+        List<String> arList = new ArrayList<>();
+        arList.add("Columbus");
+        arList.add("New York");
+        arList.add("Tokyo");
+        System.out.println(arList);
+
+        for(String city : arList){
+            System.out.println(city);
+        }
+
+        Set<String> cities2 = new TreeSet<>();
+        cities2.add("Dayton");
+        cities2.add("Toledo");
+        cities2.add("Columbus");
+
+        Map<Integer, String> hashMap = new HashMap<>();
+        hashMap.put(1, "what");
+        System.out.println(hashMap);
     }
 }
