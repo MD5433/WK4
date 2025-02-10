678604d581303386b5a462267e35429433585f05
MD5433 <mariahdungey01@gmail.com>
2025-02-10 01:11:48 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,69 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.HashMap;
 
-
+//Mariah Dungey 02/09/2025
 public class Main {
+    static Scanner userIn = new Scanner(System.in);
+    static ArrayList<String> cityNames = new ArrayList<>();
+    static HashMap<String, ArrayList<Integer>> cityTemps = new HashMap<>();
+
     public static void main(String[] args) {
-        int[] arr = new int[3];
-        arr[0] = 1;
-        arr[1] = 17;
-        arr[2] = 35;
+        userInput();
+        userCityTemps();
+        calcAvgTemps();
+        displayCityTemps();
+    }
 
-        for (int i = 0; i < 4; i++){
-            System.out.println(Arrays.toString(arr));
+    // Collects city names from user
+    static void userInput() {
+        System.out.println("Enter the city names, type 'end' to stop.");
+        String userCities = userIn.nextLine();
+        while (!userCities.equals("end")) {
+            cityNames.add(userCities);
+            userCities = userIn.nextLine();
         }
+    }
 
-        List<String> arList = new ArrayList<>();
-        arList.add("Columbus");
-        arList.add("New York");
-        arList.add("Tokyo");
-        System.out.println(arList);
-
-        for(String city : arList){
-            System.out.println(city);
+    // Collects temperatures for each city
+    static void userCityTemps() {
+        for (String city : cityNames) {
+            System.out.println("Enter temperatures for the next 5 days for the city of " + city + ".");
+            ArrayList<Integer> temperatures = new ArrayList<>();
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Day: " + i);
+                Integer userTemps = userIn.nextInt();
+                temperatures.add(userTemps);
+            }
+            cityTemps.put(city, temperatures);
+            userIn.nextLine();
         }
+    }
 
-        Set<String> cities2 = new TreeSet<>();
-        cities2.add("Dayton");
-        cities2.add("Toledo");
-        cities2.add("Columbus");
+    // Calculates average
+    static void calcAvgTemps() {
+        for (String city : cityTemps.keySet()) {
+            ArrayList<Integer> temps = cityTemps.get(city);
+            int sum = 0;
+            for (int temp : temps) {
+                sum += temp;
+            }
 
-        Map<Integer, String> hashMap = new HashMap<>();
-        hashMap.put(1, "what");
-        System.out.println(hashMap);
+            double average = sum / 5.0;
+
+            ArrayList<Integer> avgTemps = new ArrayList<>();
+            avgTemps.add((int) average);
+
+            cityTemps.put(city, avgTemps);
+        }
+    }
+
+    // Displays
+    static void displayCityTemps() {
+        for (String city : cityTemps.keySet()) {
+            ArrayList<Integer> temps = cityTemps.get(city);
+            double average = temps.get(0); // Get the average from the list
+            System.out.println("The average for " + city + " is " + average); // Display city and average
+        }
     }
 }
